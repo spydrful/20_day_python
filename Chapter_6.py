2d4e78d2fd975053ef76cfa368348e87dd83a5a9
jwallace <jwallace@spydr.io>
2023-07-12 10:15:26 -0700

    Chapter 6 in progress

---
+++ Chapter_6.py
@@ -0,0 +1,13 @@
+#Chatper 6 Dictionaires
+
+alien_0 = {'color': 'green', 'points': 5}
+
+print(alien_0['color'])
+print(alien_0['points'])
+
+#Dictionaries are basically built in XML. Its a way to store key-pair data
+
+new_points = alien_0['points']
+print(f'Your just earned {new_points} points!')
+
+#addin new key pair values
